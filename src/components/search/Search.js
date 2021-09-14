fd45dbd57a1f67150993c79551324dd7d648c99a
Alex Woodall <aww2539@gmail.com>
2021-09-14 15:14:20 -0500

    added search components to handle search state. added link property to filtered search results to take the user to the profile of selected user

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,12 +1,17 @@
+import { useState } from "react"
+import { SearchInput } from "./SearchInput"
+import { SearchOutput } from "./SearchOutput"
 
 
 export const Search = () => {
-
+    const [searchTerm, updateSearchTerm] = useState("")
 
 
     return (
         <>
             <h2>Search CreatorLink</h2>
+            <SearchInput updateSearchState={updateSearchTerm}/>
+            <SearchOutput searchState={searchTerm}/>
         </>
     )
 }
